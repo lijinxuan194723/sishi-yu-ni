605da94b1fd79a0f6a649ff2f4cca8f86b3da89c
Jinxuan Li <64257580+lijinxuan194723@users.noreply.github.com>
2026-09-10 17:40:50 +0800

    style: load startup splash styles on Android

--- mobile/main.tsx
+++ mobile/main.tsx
@@ -4,4 +4,5 @@ import Page from '../app/page';
 import '../app/globals.css';
 import '../app/ambience.css';
 import '../app/notes-polish.css';
+import '../app/startup-splash.css';
 createRoot(document.getElementById('root')!).render(<Page/>);
